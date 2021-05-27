51ce51bb76d0d64a0f185f3d55be2802f1705a0c
Joel Ring <joel.ring@gmail.com>
2021-05-27 13:46:04 +0200

    adds drawing of playing field

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -2,28 +2,52 @@
 //
 
 #include <iostream>
+#include <sstream>
 #include "tictactoe.h" 
 using namespace std;
 
 int main()
 {
-    char playField[9];
+    char playField[9] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+    int currentPlayer = 0, player1Score = 0, player2Score = 0;
 
-    for (int i=0; i< 9; i++)
-    {
-        playField[i] = ' ';
-    }
+    initGame();
+
+    int numPlayers = getNumberFromPlayer("Enter Number of Players (1/2): ", minNumberOfPlayers, maxNumberOfPlayers);
+    cout << "You choose " << numPlayers << " number of players" << endl;
+
     while (true) {
 
-        //drawPlayingField();
-        int playerSelection = getNumberFromPlayer("choose a box for your placement: ", 1, 9);
+        drawPlayingField(playField);
+        int playerSelection = 0;
+        if (numPlayers == 2 || currentPlayer == 0)
+        {
+            std::ostringstream oss;
+            oss << "Player " << currentPlayer + 1 << ", choose a box:";
+            std::string instruction = oss.str();
 
-        cout << "you chose: " << playerSelection << endl;
-        cout << endl;
+            playerSelection = getNumberFromPlayer(instruction, 1, 9);
+            cout << "you chose: " << playerSelection << endl;
+            cout << endl;
+        }
+        else //ai's turn
+        {
+            /* generate secret number between 1 and 9: */
+            int random = rand() % 9 + 1;
+            playerSelection = random;
+            cout << "AI chooses " << playerSelection << " matches\n";
+        }
+
+        currentPlayer++;
+        currentPlayer &= 0x1;
+
+
+        if (CalculateWinner())
+        {
+            break;
+        }
+
     }
-
-
-
 }
 
 int getNumberFromPlayer(string instruction, int minSelection, int maxSelection)
@@ -40,4 +64,33 @@ int getNumberFromPlayer(string instruction, int minSelection, int maxSelection)
         }
     }
     return playerSelection;
+}
+
+
+void initGame()
+{
+    cout << GREEN;
+    /* initialize random seed: */
+    srand(time(NULL));
+
+    cout << "Welcome to Tic-Tac-Toe!\n";
+    cout << "Your objective is to get three in a row.\n";
+}
+
+bool CalculateWinner()
+{
+    return false;
+}
+
+void drawPlayingField(char* playField)
+{
+    cout << endl << "-------" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "|";
+        for (int j = 0; j < 3; j++) {
+            cout << playField[i+j] << "|";;
+        }
+        cout << endl<< "-------"<<endl;
+
+    }
 }
