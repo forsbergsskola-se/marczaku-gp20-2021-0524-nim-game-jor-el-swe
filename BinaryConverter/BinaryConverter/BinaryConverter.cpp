e3a34b1bdf9816cfa4389883a06b284d692b8743
Joel Ring <joel.ring@gmail.com>
2021-05-27 11:50:32 +0200

    adds sanity check

--- BinaryConverter/BinaryConverter/BinaryConverter.cpp
+++ BinaryConverter/BinaryConverter/BinaryConverter.cpp
@@ -2,24 +2,36 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+#include "binary.h"
 using namespace std;
+/*
+Write a Program that reads (cin) decimal Integer numbers (positive only) And Oututs the Binary representation of that number: 0b110
 
+Firstly, implement the function the way we know, with division and modulo.
+Secondly, implement the function using binary operations.
+Tip: Both exercises will be a lot easier, if you output one character at a time!
+*/
 int main()
 {
-    std::cout << "Hello World!\n";
+    int intToConvert = getNumberFromPlayer("Enter a number to convert to binary representation ", 0, INT32_MAX);
+    cout << "You want to convert: " << intToConvert << endl;
 }
 
 int getNumberFromPlayer(string instruction, int minSelection, int maxSelection)
 {
     int playerSelection = -1;
-    while (playerSelection < minSelection || playerSelection > maxSelection)
+    bool validInput = false;
+    while (!validInput || playerSelection < minSelection || playerSelection > maxSelection)
     {
+        validInput = true;
         cout << instruction;
         cin >> playerSelection;
         if (!cin)
         {
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            validInput = false;
         }
     }
     return playerSelection;
---
+++ BinaryConverter/BinaryConverter/binary.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <iostream>
+using namespace std;
+int getNumberFromPlayer(string instruction, int minSelection, int maxSelection);
